0eeb8b9346fc781d17f1914c2ff14a7cfc1af36d
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-01 17:39:50 -0300

    class product

--- src/br/com/felipe/model/Product.java
+++ src/br/com/felipe/model/Product.java
@@ -2,4 +2,33 @@ package br.com.felipe.model;
 
 public class Product {
     private String firstName;
+    private Integer value;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    private String type;
+
+
 }
