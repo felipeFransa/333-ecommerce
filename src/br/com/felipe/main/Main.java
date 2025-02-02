940f3e8d757a5f80e0ff9ac8f41ed930aded4b81
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-02 20:00:26 -0300

    add product main

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -1,7 +1,14 @@
 package br.com.felipe.main;
 
+import br.com.felipe.model.Product;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("333-ecommerce");
+        Product product = new Product();
+
+        product.setFirstName("");
+        product.setValue(0);
+        product.setType("");
     }
 }
