61d75e1aac469d7065619f9e9602771e48318dd0
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-03 18:47:18 -0300

    scanner reading

--- src/br/com/felipe/main/Main.java
+++ src/br/com/felipe/main/Main.java
@@ -2,13 +2,22 @@ package br.com.felipe.main;
 
 import br.com.felipe.model.Product;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner reading = new Scanner(System.in);
         System.out.println("333-ecommerce");
         Product product = new Product();
 
-        product.setFirstName("");
+        System.out.println("enter your name product:");
+        String nameProduct = reading.nextLine();
+
+        product.setFirstName(nameProduct);
         product.setValue(0);
         product.setType("");
+
+        System.out.println(product.getFirstName());
     }
 }
