e4c30c3c2ddf795e272f35f4b42bfd73fd6803b4
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-05 18:18:27 -0300

    client structure class

--- src/br/com/felipe/model/Client.java
+++ src/br/com/felipe/model/Client.java
@@ -1,4 +1,49 @@
 package br.com.felipe.model;
 
 public class Client {
+    private String firstName;
+    private String city;
+    private String region;
+    private Integer phoneNumber;
+    private Integer cep;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
 }
