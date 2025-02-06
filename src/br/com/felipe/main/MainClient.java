3d3559b80523a67c113d12dacae2c9b526760b9f
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-06 19:45:41 -0300

    main client

---
+++ src/br/com/felipe/main/MainClient.java
@@ -0,0 +1,9 @@
+package br.com.felipe.main;
+
+import br.com.felipe.model.Client;
+
+public class MainClient {
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
+}
