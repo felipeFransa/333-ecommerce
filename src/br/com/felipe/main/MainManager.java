c7e01da3b1a2d0986fad7e376d66e4ff87f08baa
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-13 17:46:40 -0300

    main manager

---
+++ src/br/com/felipe/main/MainManager.java
@@ -0,0 +1,9 @@
+package br.com.felipe.main;
+
+import java.util.Scanner;
+
+public class MainManager {
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+    }
+}
