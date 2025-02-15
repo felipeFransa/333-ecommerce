f101f29395de85b4092da2922b1dcad538fa0a99
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-15 18:27:31 -0300

    2 teste

--- src/br/com/felipe/main/MainManager.java
+++ src/br/com/felipe/main/MainManager.java
@@ -1,9 +1,12 @@
 package br.com.felipe.main;
 
+import br.com.felipe.model.Manager;
+
 import java.util.Scanner;
 
 public class MainManager {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
+        Manager manager = new Manager();
     }
 }
