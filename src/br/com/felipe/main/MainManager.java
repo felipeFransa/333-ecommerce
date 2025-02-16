e4c4de1ba163afaa239c3ced48ef91f4fdb7bb1b
Felipe Fransa <antoniofelipesilva@live.com>
2025-02-16 18:41:37 -0300

    manager set/get

--- src/br/com/felipe/main/MainManager.java
+++ src/br/com/felipe/main/MainManager.java
@@ -8,5 +8,15 @@ public class MainManager {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
         Manager manager = new Manager();
+
+        System.out.println("Enter your name:");
+        String name = reading.nextLine();
+        System.out.println("Enter your city:");
+        String city = reading.nextLine();
+
+        manager.setFirstName(name);
+        manager.setAge(28);
+        manager.setType(true);
+        manager.setCity(city);
     }
 }
