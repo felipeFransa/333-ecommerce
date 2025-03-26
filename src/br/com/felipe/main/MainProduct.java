f48000e12ab16525ecc4ed762362a6bdd791ebd8
FelipeFransa <antoniofelipesilva@live.com>
2025-03-26 18:55:58 -0300

    main product

--- src/br/com/felipe/main/MainProduct.java
+++ src/br/com/felipe/main/MainProduct.java
@@ -1,7 +1,12 @@
 package br.com.felipe.main;
 
+import br.com.felipe.model.Product;
+
 public class MainProduct {
     public static void main(String[] args) {
+        Product product = new Product();
 
+        product.setName("");
+        product.setDescription("");
     }
 }
