847e49f37826faf3fe671de01b3e5edb3df79c70
Felipe Fransa <antoniofelipesilva@live.com>
2025-03-31 17:58:11 -0300

    function works

--- src/br/com/felipe/model/Works.java
+++ src/br/com/felipe/model/Works.java
@@ -4,4 +4,20 @@ public class Works {
 
     private String title;
     private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
