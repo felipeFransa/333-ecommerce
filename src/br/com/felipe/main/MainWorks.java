10ce7051cf8c491e0275bd0e87cf917cee6eb37f
Felipe Fransa <antoniofelipesilva@live.com>
2025-04-01 18:41:09 -0300

    main works

---
+++ src/br/com/felipe/main/MainWorks.java
@@ -0,0 +1,9 @@
+package br.com.felipe.main;
+
+import br.com.felipe.model.Works;
+
+public class MainWorks {
+    public static void main(String[] args) {
+        Works works = new Works();
+    }
+}
