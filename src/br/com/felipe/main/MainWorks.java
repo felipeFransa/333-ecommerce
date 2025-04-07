d058cf88c14e7c32b4a800db4b5af56349bb2405
felipe <antoniofelipesilva@live.com>
2025-04-07 17:25:46 -0300

    main works date/time

--- src/br/com/felipe/main/MainWorks.java
+++ src/br/com/felipe/main/MainWorks.java
@@ -2,9 +2,17 @@ package br.com.felipe.main;
 
 import br.com.felipe.model.Works;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class MainWorks {
     public static void main(String[] args) {
         Works works = new Works();
+        LocalDate currentDate = LocalDate.now();
+        LocalTime currentTime = LocalTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
         works.setTitle("");
         works.setDescription("");
--- src/br/com/felipe/model/Works.java
+++ src/br/com/felipe/model/Works.java
@@ -1,9 +1,9 @@
 package br.com.felipe.model;
 
 public class Works {
-
     private String title;
     private String description;
+
 
     public String getTitle() {
         return title;
