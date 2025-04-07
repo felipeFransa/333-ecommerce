418406f7fb962973066972a6b68b3c7704a6635b
felipe <antoniofelipesilva@live.com>
2025-04-06 21:19:29 -0300

    class main works

--- src/br/com/felipe/main/MainWorks.java
+++ src/br/com/felipe/main/MainWorks.java
@@ -5,5 +5,11 @@ import br.com.felipe.model.Works;
 public class MainWorks {
     public static void main(String[] args) {
         Works works = new Works();
+
+        works.setTitle("");
+        works.setDescription("");
+
+        System.out.printf(works.getTitle());
+        System.out.printf(works.getDescription());
     }
 }
