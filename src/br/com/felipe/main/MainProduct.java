30c10c0401fa5f012133c999c91180e13874deaa
felipe <antoniofelipesilva@live.com>
2025-04-11 17:11:28 -0300

    main product scanner

--- src/br/com/felipe/main/MainProduct.java
+++ src/br/com/felipe/main/MainProduct.java
@@ -2,12 +2,21 @@ package br.com.felipe.main;
 
 import br.com.felipe.model.Product;
 
+import java.util.Scanner;
+
 public class MainProduct {
     public static void main(String[] args) {
         Product product = new Product();
+        Scanner reading = new Scanner(System.in);
 
-        product.setName("");
-        product.setDescription("");
+        System.out.println("Enter your name product:");
+        String nameProduct = reading.nextLine();
+
+        System.out.println("Enter your description:");
+        String description = reading.nextLine();
+
+        product.setName(nameProduct);
+        product.setDescription(description);
 
         System.out.println(product.getName());
         System.out.println(product.getDescription());
