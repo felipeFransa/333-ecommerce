4071c35af87914e44e2b8f5230265361a1ed194a
Felipe Fransa <antoniofelipesilva@live.com>
2025-04-15 17:15:08 -0300

    scanner add title/description

--- src/br/com/felipe/main/MainWorks.java
+++ src/br/com/felipe/main/MainWorks.java
@@ -5,17 +5,25 @@ import br.com.felipe.model.Works;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class MainWorks {
     public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
         Works works = new Works();
         LocalDate currentDate = LocalDate.now();
         LocalTime currentTime = LocalTime.now();
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-        works.setTitle("");
-        works.setDescription("");
+        System.out.println("Enter your title");
+        String titleWork = reading.nextLine();
+
+        System.out.println("enter your description");
+        String descriptionWork = reading.nextLine();
+
+        works.setTitle(titleWork);
+        works.setDescription(descriptionWork);
 
         System.out.printf(works.getTitle());
         System.out.printf(works.getDescription());
